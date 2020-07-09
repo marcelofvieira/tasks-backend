3849edfc9492b32e643e4414efedac644496ccc1
marcelofvieira <marcelofvieira@gmail.com>
2020-07-08 23:33:03 -0300

    Ajuste teste

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,32 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+	@Test
+	public void testFutureDate() {
+		LocalDate date = LocalDate.of(2030, 1, 1);
+		assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+
+	
+	@Test
+	public void testPasteDate () {
+		LocalDate date = LocalDate.of(2010, 1, 1);
+		assertFalse(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	
+	@Test
+	public void testActualDate() {
+		LocalDate date = LocalDate.now();
+		assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+}
---
+++ src/test/java/br/ce/wcaquino/tasksfrontend/controller/TaskControllerTest.java
@@ -0,0 +1,101 @@
+package br.ce.wcaquino.tasksfrontend.controller;
+
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.controller.TaskController;
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+import junit.framework.Assert;
+
+public class TaskControllerTest {
+
+	@Mock
+	private TaskRepo taskRepo;
+	
+	@InjectMocks
+	private TaskController controller;
+	
+	
+	@Before
+	public void setup() {
+		
+		MockitoAnnotations.initMocks(this);		
+	}
+	
+	@Test
+	public void descriptionIsEmpty () {
+		
+		Task task = new Task();
+		
+		task.setDueDate(LocalDate.now());
+		
+		try {
+		
+			controller.save(task);
+		
+			Assert.fail("Deveria ter ocorrido erro"); 
+		
+		}
+		catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void dateIsEmpty() {
+		Task task = new Task();
+		
+		task.setTask("Teste de tarefa");
+		
+		try {
+		
+			controller.save(task);
+			
+			Assert.fail("Deveria ter ocorrido erro");
+		}
+		catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void noPastDate() {
+		Task task = new Task();
+		
+		task.setTask("Teste de tarefa");
+		task.setDueDate(LocalDate.of(2010, 1, 1) );
+		
+		try {
+		
+			controller.save(task);
+			
+			Assert.fail("Deveria ter ocorrido erro");
+		}
+		catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void goodData () throws ValidationException {
+		Task task = new Task();
+		
+		task.setDueDate(LocalDate.of(2030, 1, 1) );
+		task.setTask("Tarefa Ok");
+		
+		controller.save(task);
+		
+		Mockito.verify(taskRepo).save(task);
+		
+
+	}
+	
+}
